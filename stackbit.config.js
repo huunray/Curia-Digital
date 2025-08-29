c22773f8d618b302938ab62c0ee4365d6c0c18cf
huunray <eguakunosakhare99@gmail.com>
2025-08-29 17:18:35 +0100

    Changes to stackbit.config.js file

--- stackbit.config.js
+++ stackbit.config.js
@@ -1,47 +1,30 @@
 import { ContentfulContentSource } from "@stackbit/cms-contentful";
 
+import { ContentfulContentSource } from "@stackbit/cms-contentful";
+
+import { ContentfulContentSource } from "@stackbit/cms-contentful";
+
+new ContentfulContentSource({
+  spaceId: "l6ndvf8nwdyg",
+  environment: "master-2025-08-27",
+  previewToken: "HCbDkRzozrMb89p4bayZkMksBckUHnbL6OpyfiOkyaM",
+  accessToken: "HCbDkRzozrMb89p4bayZkMksBckUHnbL6OpyfiOkyaM",
+  useWebhookForContentUpdates: true,
+});
+
 export default {
   stackbitVersion: "~0.6.0",
   ssgName: "nextjs",
   nodeVersion: "16",
   contentSources: [
     new ContentfulContentSource({
-      spaceId: process.env.l6ndvf8nwdyg,
-      environment: process.env.T14-20-00-00Z,
-      previewToken: process.env.HCbDkRzozrMb89p4bayZkMksBckUHnbL6OpyfiOkyaM,
-      accessToken: process.env.MeuIve6xZrZf-U61rwW3A-zFfEfeFDXKk1ds6Xi-XLQ,
+      spaceId: process.env.CONTENTFUL_SPACE_ID,
+      environment: process.env.CONTENTFUL_ENVIRONMENT,
+      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN,
+      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
     }),
   ],
   models: {
     page: { type: "page", urlPath: "/{slug}" },
   },
 };
-
-// stackbit.config.ts
-import { defineStackbitConfig } from "@stackbit/types";
-import { GitContentSource } from "@stackbit/cms-git";
-
-export default defineStackbitConfig({
-  stackbitVersion: "~0.6.0",
-  contentSources: [
-    new GitContentSource({
-      rootPath: __dirname,
-      contentDirs: ["content"],
-      models: [
-        {
-          name: "Page",
-          type: "page",
-          urlPath: "/{slug}",
-          filePath: "content/pages/{slug}.json",
-          fields: [{ name: "title", type: "string", required: true }]
-        }
-      ],
-      assetsConfig: {
-        referenceType: "static",
-        staticDir: "public",
-        uploadDir: "images",
-        publicPath: "/"
-      }
-    })
-  ]
-});
