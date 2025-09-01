338ead79df35762fb7880e03b58d647a1cbc268b
huunray <eguakunosakhare99@gmail.com>
2025-09-01 10:00:57 +0100

    Eleventy Option

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function (eleventyConfig) {
+  // Copy Webflow asset folders straight through
+  eleventyConfig.addPassthroughCopy({ "src/css": "css" });
+  eleventyConfig.addPassthroughCopy({ "src/js": "js" });
+  eleventyConfig.addPassthroughCopy({ "src/images": "images" });
+
+  // (Add more passthroughs if you have fonts/, assets/, etc.)
+  // eleventyConfig.addPassthroughCopy({ "src/fonts": "fonts" });
+
+  return {
+    dir: { input: "src", output: "_site" },
+  };
+};
