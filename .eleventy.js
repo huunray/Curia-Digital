ca84b0919a633aea58327b07758ab80c52843214
huunray <eguakunosakhare99@gmail.com>
2025-09-01 10:09:57 +0100

    DEFAULT Changes

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,8 @@
 module.exports = function (eleventyConfig) {
   // Copy Webflow asset folders straight through
-  eleventyConfig.addPassthroughCopy({ "src/css": "css" });
-  eleventyConfig.addPassthroughCopy({ "src/js": "js" });
-  eleventyConfig.addPassthroughCopy({ "src/images": "images" });
+  eleventyConfig.addPassthroughCopy({ "/css": "css" });
+  eleventyConfig.addPassthroughCopy({ "/js": "js" });
+  eleventyConfig.addPassthroughCopy({ "/images": "images" });
 
   // (Add more passthroughs if you have fonts/, assets/, etc.)
   // eleventyConfig.addPassthroughCopy({ "src/fonts": "fonts" });
